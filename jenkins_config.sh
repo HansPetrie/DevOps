48b30dcf3a0af42b78a8e2e7096ead0342163fa8
root <root@ip-172-31-58-63.ec2.internal>
2015-09-16 21:49:56 +0000

    Added code pipeline job

--- jenkins_config.sh
+++ jenkins_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 wget -O /var/lib/jenkins/plugins/aws-codepipeline-for-jenkins.hpi https://s3-us-west-2.amazonaws.com/publicrandomstuff/aws-codepipeline-for-jenkins.hpi
+mkdir -p /var/lib/jenkins/jobs/MyDemoProject
+cp config.xml /var/lib/jenkins/jobs/MyDemoProject
 service jenkins restart
 
