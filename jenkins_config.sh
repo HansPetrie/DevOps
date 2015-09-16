2ee3a8c8a1927811ff01a4080255a954bfc888b1
root <root@ip-172-31-58-57.ec2.internal>
2015-09-16 22:04:04 +0000

    Fixed permissions for jenkins job configuration

--- jenkins_config.sh
+++ jenkins_config.sh
@@ -3,5 +3,6 @@
 wget -O /var/lib/jenkins/plugins/aws-codepipeline-for-jenkins.hpi https://s3-us-west-2.amazonaws.com/publicrandomstuff/aws-codepipeline-for-jenkins.hpi
 mkdir -p /var/lib/jenkins/jobs/MyDemoProject
 cp config.xml /var/lib/jenkins/jobs/MyDemoProject
+chown -R jenkins:jenkins /var/lib/jenkins/jobs/MyDemoProject
 service jenkins restart
 
