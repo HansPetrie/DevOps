d5b85e5864e8fd06bbec8e265ea76ebdaa747a79
root <root@ip-172-31-53-246.ec2.internal>
2015-09-16 21:22:54 +0000

    Removed plugin

--- jenkins.sh
+++ jenkins.sh
@@ -4,6 +4,5 @@ yum update -y
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
 yum -y install jenkins
-wget -O /var/lib/jenkins/plugins/aws-codepipeline-for-jenkins.hpi https://s3-us-west-2.amazonaws.com/publicrandomstuff/aws-codepipeline-for-jenkins.hpi
 service jenkins start
 chkconfig jenkins on
