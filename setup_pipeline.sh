ce494d5d5209d02602eef4160b82132e4ff8fbff
root <root@ip-172-31-49-88.ec2.internal>
2015-09-17 18:37:23 +0000

    Fixed create pipeline scripts

--- setup_pipeline.sh
+++ setup_pipeline.sh
@@ -1,2 +1,11 @@
-aws --region us-east-1 codepipeline create-custom-action-type --cli-input-json file://custom_action_type.json
-aws --region us-east-1 codepipeline create-pipeline --cli-input-json file://newpipeline.json
+HOSTNAME=$(curl -s 169.254.169.254/latest/meta-data/public-hostname/)
+echo $HOSTNAME
+VERSION=$(date +%y%m%d)
+echo $VERSION
+
+sed "s|JENKINS_SERVER|$HOSTNAME|g" custom_action_type.json > /tmp/custom_action_type.json
+sed -i "s|VERSION_STRING|$VERSION|g" /tmp/custom_action_type.json
+aws --region us-east-1 codepipeline create-custom-action-type --cli-input-json file:///tmp/custom_action_type.json
+
+sed "s|VERSION_STRING|$VERSION|g" newpipeline.json > /tmp/newpipeline.json
+aws --region us-east-1 codepipeline create-pipeline --cli-input-json file:///tmp/newpipeline.json
