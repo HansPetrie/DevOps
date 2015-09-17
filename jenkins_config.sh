43e728e597262d45fd022ca4be25d3c9726a99f5
root <root@ip-172-31-63-57.ec2.internal>
2015-09-17 19:36:41 +0000

    Give jenkins 3 minutes to warm up

--- jenkins_config.sh
+++ jenkins_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Sleep allows Jenkins time to start up and extract itself
+sleep 180
 wget -O /var/lib/jenkins/plugins/aws-codepipeline-for-jenkins.hpi https://s3-us-west-2.amazonaws.com/publicrandomstuff/aws-codepipeline-for-jenkins.hpi
 mkdir -p /var/lib/jenkins/jobs/MyDemoProject
 cp config.xml /var/lib/jenkins/jobs/MyDemoProject
