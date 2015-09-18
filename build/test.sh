d86d9a9e88aef47ab63f4f10ead545963bc0ef69
root <root@ip-172-31-50-86.ec2.internal>
2015-09-18 18:51:00 +0000

    Added pipeline build demo

---
+++ build/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+STACKNAME=test$(date +%Y%m%d%H%M%S)
+
+aws --region us-east-1 cloudformation create-stack --stack-name $STACKNAME --template-body file://teststack.json --parameters ParameterKey=DBPassword,ParameterValue=password ParameterKey=DBRootPassword,ParameterValue=password ParameterKey=KeyName,ParameterValue=default ParameterKey=DBUser,ParameterValue=admin
+
+while [ `aws --region us-east-1 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].[StackStatus]'` != "CREATE_COMPLETE" ]
+do
+  sleep 30
+  echo "Stack Building." 
+done
+
+aws --region us-east-1 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].Outputs[*].OutputValue'
+
