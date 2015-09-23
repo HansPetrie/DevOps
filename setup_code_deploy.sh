770d7cc73d0c9ecc20a631096a989ba4bcda530f
root <root@ip-172-31-52-111.ec2.internal>
2015-09-23 19:12:26 +0000

    Overhaul of codedeploy

--- setup_code_deploy.sh
+++ setup_code_deploy.sh
@@ -4,6 +4,12 @@ source /DevOps/devops.cnf
 
 aws --region us-east-1 deploy create-application --application-name WordPress_App
 
+wget -q -O /tmp https://s3-us-west-2.amazonaws.com/publicrandomstuff/WordPress.zip
+
+unzip /tmp/WordPress.zip
+
+cd /tmp/WordPress
+
 aws --region us-east-1 deploy push --application-name WordPress_App --s3-location s3://$DevopsBucketName/WordPressApp.zip --ignore-hidden-files
 
 aws --region us-east-1 deploy create-deployment-group --application-name WordPress_App --deployment-group-name WordPress_DepGroup --deployment-config-name CodeDeployDefault.OneAtATime --ec2-tag-filters Key=Name,Value=CodeDeployDemo,Type=KEY_AND_VALUE --service-role-arn $CodeDeployRoleArn 
