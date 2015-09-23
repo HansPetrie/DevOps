bfc17b536146d36c6ccd871d7d9a07f8093ff30e
root <root@ip-172-31-9-157.ec2.internal>
2015-09-23 22:25:57 +0000

    Delete old pipelines

--- setup_pipeline.sh
+++ setup_pipeline.sh
@@ -21,5 +21,9 @@ aws --region us-east-1 codepipeline create-custom-action-type --cli-input-json f
 sed "s|VERSION_STRING|$VersionString|g" newpipeline.json > /tmp/newpipeline.json
 sed -i "s|ROLEARN|$CodePipelineRoleArn|g" /tmp/newpipeline.json
 sed -i "s|DEVOPSBUCKET|$DevopsBucketName|g" /tmp/newpipeline.json
+sed -i "s|CODEPIPELINENAME|$CodePipelineName|g" /tmp/newpipeline.json
+
+#If the pipeline already exists delete it and re-create it with the updated custom_action_type
+aws --region us-east-1 codepipeline get-pipeline --name $CodePipelineName && aws --region us-east-1 codepipeline delete-pipeline --name $CodePipelineName
 
 aws --region us-east-1 codepipeline create-pipeline --cli-input-json file:///tmp/newpipeline.json
