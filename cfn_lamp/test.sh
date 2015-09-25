2ab40101694e4ff044d72435dda245a11b7ed579
root <root@ip-172-31-44-28.ec2.internal>
2015-09-25 20:43:50 +0000

    Made the usernames and passwords the wordpress defualt

--- cfn_lamp/test.sh
+++ cfn_lamp/test.sh
@@ -2,7 +2,7 @@
 
 STACKNAME=test$(date +%Y%m%d%H%M%S)
 
-aws --region us-east-1 cloudformation create-stack --stack-name $STACKNAME --template-body file://teststack.json --parameters ParameterKey=DBPassword,ParameterValue=password ParameterKey=DBRootPassword,ParameterValue=password ParameterKey=KeyName,ParameterValue=default ParameterKey=DBUser,ParameterValue=admin --capabilities CAPABILITY_IAM 
+aws --region us-east-1 cloudformation create-stack --stack-name $STACKNAME --template-body file://teststack.json --parameters ParameterKey=DBPassword,ParameterValue=password ParameterKey=DBRootPassword,ParameterValue=password ParameterKey=KeyName,ParameterValue=default ParameterKey=DBUser,ParameterValue=username --capabilities CAPABILITY_IAM 
 
 while [ `aws --region us-east-1 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].[StackStatus]'` != "CREATE_COMPLETE" ]
 do
