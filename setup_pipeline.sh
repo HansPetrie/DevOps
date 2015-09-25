cb646479fc6f186c6cacbf4ea649d0c156a541d8
root <root@ip-172-31-36-207.ec2.internal>
2015-09-25 00:16:50 +0000

    Do Jenkins setup after pipeline to avoid double build

--- setup_pipeline.sh
+++ setup_pipeline.sh
@@ -4,15 +4,6 @@ source devops.cnf
 
 # Sleep allows Jenkins time to start up and extract itself
 sleep 30 
-wget -q -O /var/lib/jenkins/plugins/aws-codepipeline-for-jenkins.hpi https://s3-us-west-2.amazonaws.com/publicrandomstuff/aws-codepipeline-for-jenkins.hpi
-mkdir -p /var/lib/jenkins/jobs/MyDemoProject
-
-sed "s|VERSION_STRING|$VersionString|g" config.xml > /tmp/config.xml
-
-cp /tmp/config.xml /var/lib/jenkins/jobs/MyDemoProject
-chown -R jenkins:jenkins /var/lib/jenkins/jobs/MyDemoProject
-service jenkins restart
-
 
 sed "s|JENKINS_SERVER|$JenkinsHostname|g" custom_action_type.json > /tmp/custom_action_type.json
 sed -i "s|VERSION_STRING|$VersionString|g" /tmp/custom_action_type.json
@@ -27,3 +18,12 @@ sed -i "s|CODEPIPELINENAME|$CodePipelineName|g" /tmp/newpipeline.json
 aws --region us-east-1 codepipeline get-pipeline --name $CodePipelineName && aws --region us-east-1 codepipeline delete-pipeline --name $CodePipelineName
 
 aws --region us-east-1 codepipeline create-pipeline --cli-input-json file:///tmp/newpipeline.json
+
+wget -q -O /var/lib/jenkins/plugins/aws-codepipeline-for-jenkins.hpi https://s3-us-west-2.amazonaws.com/publicrandomstuff/aws-codepipeline-for-jenkins.hpi
+mkdir -p /var/lib/jenkins/jobs/MyDemoProject
+
+sed "s|VERSION_STRING|$VersionString|g" config.xml > /tmp/config.xml
+
+cp /tmp/config.xml /var/lib/jenkins/jobs/MyDemoProject
+chown -R jenkins:jenkins /var/lib/jenkins/jobs/MyDemoProject
+service jenkins restart
