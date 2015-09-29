d45c1bcc26cf6ea9e8d90671329ba4938c7c30b7
root <root@ip-172-31-52-2.ec2.internal>
2015-09-29 20:02:00 +0000

    Switched to using erb templates instead of sed

--- get_envs.sh
+++ get_envs.sh
@@ -4,7 +4,10 @@ source devops.cnf
 
 aws --region $CloudformationRegion cloudformation describe-stacks --stack-name $CloudformationStackName --output text --query Stacks[*].Parameters[*].[ParameterKey,ParameterValue] > /tmp/envs
 aws --region $CloudformationRegion cloudformation describe-stacks --stack-name $CloudformationStackName --output text --query Stacks[*].Outputs[*].[OutputKey,OutputValue] >> /tmp/envs
-sed 's/\t/=/g' /tmp/envs >> devops.cnf
+sed -i 's/\t/="/g' /tmp/envs
+sed 's/$/"/g' /tmp/envs >> devops.cnf
 
-echo "JenkinsHostname=$(curl -s 169.254.169.254/latest/meta-data/public-hostname/)" >> devops.cnf
-echo "VersionString=$(date +%m%d%H%M)" >> devops.cnf
+echo "JenkinsHostname=\"$(curl -s 169.254.169.254/latest/meta-data/public-hostname/)\"" >> devops.cnf
+echo "VersionString=\"$(date +%m%d%H%M)\"" >> devops.cnf
+
+sed "s/^/@/g" devops.cnf >> devops.rb
--- setup_pipeline.sh
+++ setup_pipeline.sh
@@ -2,28 +2,18 @@
 
 source devops.cnf
 
-sed "s|JENKINS_SERVER|$JenkinsHostname|g" custom_action_type.json > /tmp/custom_action_type.json
-sed -i "s|VERSION_STRING|$VersionString|g" /tmp/custom_action_type.json
+erb -r './devops.rb' custom_action_type.json > /tmp/custom_action_type.json
 aws --region us-east-1 codepipeline create-custom-action-type --cli-input-json file:///tmp/custom_action_type.json
 
-sed "s|VERSION_STRING|$VersionString|g" newpipeline.json > /tmp/newpipeline.json
-sed -i "s|ROLEARN|$CodePipelineRoleArn|g" /tmp/newpipeline.json
-sed -i "s|DEVOPSBUCKET|$DevopsBucketName|g" /tmp/newpipeline.json
-sed -i "s|CODEPIPELINENAME|$CodePipelineName|g" /tmp/newpipeline.json
+erb -r './devops.rb' newpipeline.json > /tmp/newpipeline.json
 
 #If the pipeline already exists delete it and re-create it with the updated custom_action_type
 aws --region us-east-1 codepipeline get-pipeline --name $CodePipelineName && aws --region us-east-1 codepipeline delete-pipeline --name $CodePipelineName
-
 aws --region us-east-1 codepipeline create-pipeline --cli-input-json file:///tmp/newpipeline.json
 
 
-sed "s|VERSION_STRING|$VersionString|g" wordpresspipeline.json > /tmp/wordpresspipeline.json
-sed -i "s|ROLEARN|$CodePipelineRoleArn|g" /tmp/wordpresspipeline.json
-sed -i "s|DEVOPSBUCKET|$DevopsBucketName|g" /tmp/wordpresspipeline.json
-sed -i "s|CODEPIPELINENAME|WordPressPipeline|g" /tmp/wordpresspipeline.json
+erb -r './devops.rb' wordpresspipeline.json > /tmp/wordpresspipeline.json
 
-#If the pipeline already exists delete it and re-create it with the updated custom_action_type
 aws --region us-east-1 codepipeline get-pipeline --name WordPressPipeline && aws --region us-east-1 codepipeline delete-pipeline --name WordPressPipeline 
-
 aws --region us-east-1 codepipeline create-pipeline --cli-input-json file:///tmp/wordpresspipeline.json
 
