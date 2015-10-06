12899e92dd007c3193e8f7fe206acfbc565d9c7c
root <root@ip-172-31-53-97.ec2.internal>
2015-10-06 21:52:47 +0000

    Added a more robust RDS template

--- cfn_rds/test.sh
+++ cfn_rds/test.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-STACKNAME=RDSMysql$(date +%Y%m%d%H%M%S)
+STACKNAME=RDSWordpress
 
 aws --region us-west-2 cloudformation create-stack --stack-name $STACKNAME --template-body file://teststack.json --capabilities CAPABILITY_IAM 
 
-exit 0
-
-while [ `aws --region us-east-2 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].[StackStatus]'` != "CREATE_COMPLETE" ]
+while [ `aws --region us-west-2 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].[StackStatus]'` != "CREATE_COMPLETE" ]
 do
   sleep 30
   echo "Stack Building." 
 done
 
-aws --region us-east-2 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].Outputs[*].OutputValue'
+aws --region us-west-2 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].Outputs[*].OutputValue'
