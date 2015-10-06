72dee1721fbc577c1132c1cf8f7a48c7dac21f32
root <root@ip-172-31-53-97.ec2.internal>
2015-10-06 20:38:32 +0000

    Fixed bug in region

--- cfn_network/test.sh
+++ cfn_network/test.sh
@@ -4,10 +4,10 @@ STACKNAME=Network$(date +%Y%m%d%H%M%S)
 
 aws --region us-west-2 cloudformation create-stack --stack-name $STACKNAME --template-body file://teststack.json --capabilities CAPABILITY_IAM 
 
-while [ `aws --region us-east-2 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].[StackStatus]'` != "CREATE_COMPLETE" ]
+while [ `aws --region us-west-2 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].[StackStatus]'` != "CREATE_COMPLETE" ]
 do
   sleep 30
   echo "Stack Building." 
 done
 
-aws --region us-east-2 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].Outputs[*].OutputValue'
+aws --region us-west-2 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].Outputs[*].OutputValue'
