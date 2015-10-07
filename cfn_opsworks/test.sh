f77490d16299f9fe535f6786d2fe16bbc7d2b978
root <root@ip-172-31-53-97.ec2.internal>
2015-10-07 18:32:30 +0000

    Added a cfn for opsworks

---
+++ cfn_opsworks/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+STACKNAME=OpsworksTest
+
+aws --region us-east-1 cloudformation create-stack --stack-name $STACKNAME --template-body file://teststack.json --parameters ParameterKey=DBPassword,ParameterValue=password ParameterKey=DBRootPassword,ParameterValue=password ParameterKey=KeyName,ParameterValue=default ParameterKey=DBUser,ParameterValue=username --capabilities CAPABILITY_IAM 
+
+while [ `aws --region us-east-1 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].[StackStatus]'` != "CREATE_COMPLETE" ]
+do
+  sleep 30
+  echo "Stack Building." 
+done
+
+aws --region us-east-1 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].Outputs[*].OutputValue'
+
