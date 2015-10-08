471e8c2b0be79ceaa51919adb45918adcb34ef6e
root <root@ip-172-31-53-97.ec2.internal>
2015-10-08 21:28:56 +0000

    Minor fixes for jenkins

--- cfn_opsworks/test.sh
+++ cfn_opsworks/test.sh
@@ -2,7 +2,7 @@
 
 STACKNAME=OpsworksTest
 
-aws --region us-east-1 cloudformation create-stack --stack-name $STACKNAME --template-body file://teststack.json --parameters ParameterKey=MysqlRootPassword,ParameterValue=password --capabilities CAPABILITY_IAM 
+aws --region us-east-1 cloudformation create-stack --stack-name $STACKNAME --template-body file://teststack.json --capabilities CAPABILITY_IAM 
 
 while [ `aws --region us-east-1 cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].[StackStatus]'` != "CREATE_COMPLETE" ]
 do
