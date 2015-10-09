cca3e57d02ab04e0cb1a7436d063c35613cf9008
root <root@ip-172-31-60-72.ec2.internal>
2015-10-09 22:47:36 +0000

    Cleanup

--- setup_bucket.sh
+++ setup_bucket.sh
@@ -2,6 +2,8 @@
 
 source devops.cnf
 
+zip -r /tmp/test.zip cfn_lamp/*
+
 aws --region us-east-1 s3 ls s3://$DevopsBucketName || aws s3 mb s3://$DevopsBucketName
 aws --region us-east-1 s3api put-bucket-versioning --bucket $DevopsBucketName --versioning-configuration Status=Enabled
-aws --region us-east-1 s3 cp cfn_lamp/test.zip s3://$DevopsBucketName
+aws --region us-east-1 s3 cp /tmp/test.zip s3://$DevopsBucketName
