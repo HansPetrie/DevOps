a2d7a80743bf202b04327463ee6bf5ef49e4daf8
root <root@ip-172-31-60-72.ec2.internal>
2015-10-09 23:16:20 +0000

    Added a small fix for rundeck

--- rundeck.sh
+++ rundeck.sh
@@ -2,5 +2,6 @@
 
 rpm -Uvh https://s3-us-west-2.amazonaws.com/publicrandomstuff/rundeck.rpm
 yum -y install rundeck
+cp rendered_templates/rundeck-config.properties /etc/rundeck
 service rundeckd start
 chkconfig rundeckd on
