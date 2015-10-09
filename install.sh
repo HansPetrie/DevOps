a1085d12d8395d5fc594cbf952053db3b0831ccc
hpetrie <hans.petrie@lookout.com>
2015-10-09 15:32:56 -0700

    Changed order of install so bucket is ready

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ./get_envs.sh
 ./render_templates.sh
+./setup_bucket.sh
 ./setup_repo.sh
 ./setup_git.sh
 ./jenkins.sh
@@ -9,6 +10,5 @@
 ./configure_jenkins.sh
 ./rundeck.sh
 ./setup_code_deploy.sh
-./setup_bucket.sh
 ./setup_pipeline.sh
 ./code_deploy_agent.sh
