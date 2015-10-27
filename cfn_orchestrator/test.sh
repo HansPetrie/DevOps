8e2316924cf70c7e7763f411955f820be8761c41
hpetrie <hans.petrie@lookout.com>
2015-10-27 11:53:29 -0700

    Added fixes

--- cfn_orchestrator/test.sh
+++ cfn_orchestrator/test.sh
@@ -3,6 +3,14 @@
 REGION="us-east-1"
 STACKNAME="DevOps"
 
+aws --region $REGION cloudformation delete-stack --stack-name $STACKNAME
+
+while aws --region $REGION cloudformation describe-stacks --stack-name $STACKNAME
+do
+  sleep 30
+  echo "Stack Deleting." 
+done
+
 aws --region $REGION cloudformation create-stack --stack-name $STACKNAME --template-body file://orchestrator.json --parameters ParameterKey=KeyName,ParameterValue=default --capabilities CAPABILITY_IAM 
 
 while [ `aws --region $REGION cloudformation describe-stacks --stack-name $STACKNAME --output text --query 'Stacks[*].[StackStatus]'` != "CREATE_COMPLETE" ]
--- setup_bucket.sh
+++ setup_bucket.sh
@@ -2,7 +2,8 @@
 
 source devops.cnf
 
-zip -r /tmp/test.zip cfn_lamp/*
+cd cfn_lamp
+zip -r /tmp/test.zip *
 
 aws --region us-east-1 s3 ls s3://$DevopsBucketName || aws s3 mb s3://$DevopsBucketName
 aws --region us-east-1 s3api put-bucket-versioning --bucket $DevopsBucketName --versioning-configuration Status=Enabled
