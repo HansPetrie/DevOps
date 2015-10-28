3581d0a98b10207c4c529203c1760e1de1530c19
root <root@ip-172-31-46-224.ec2.internal>
2015-10-28 22:29:49 +0000

    New Jenkins Jobs and refactor

--- configure_jenkins.sh
+++ configure_jenkins.sh
@@ -11,10 +11,11 @@ do
 done
 
 java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin /tmp/aws-codepipeline-for-jenkins.hpi
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job MyDemoProject < rendered_templates/config.xml
+java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job MyDemoProject < rendered_templates/mydemoproject.xml
 java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job DeployLampServer < rendered_templates/deploy_lamp_jenkins_job.xml
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job DeployProductionNetwork < rendered_templates/deploy_prod_network_job.xml
+java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job DeployNetworks < rendered_templates/deploy_networks.xml
 java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job DeployOpsworks < rendered_templates/deploy_opsworks_job.xml
+java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job DeployRDSMysql < rendered_templates/deploy_rds.xml
 
 # A restart is inevitable here because the AWS CodePipeline Plugin install doesn't work until Jenkins is restarted
 service jenkins restart
