65de147249c0154f6467df8f9a516e7fd6dc6ea5
root <root@ip-172-31-46-224.ec2.internal>
2015-10-28 22:46:32 +0000

    Changed networks to stage

--- deploy_rds.sh
+++ deploy_rds.sh
@@ -1,4 +1,4 @@
-./deploy_stack.sh cloudformation/rdsmysql.json RDSProduction us-east-1 "ParameterKey=NetworkStackName,ParameterValue=ProductionNetwork"
+./deploy_stack.sh cloudformation/rdsmysql.json RDSProduction us-east-1 "ParameterKey=NetworkStackName,ParameterValue=networkstage"
 aws s3 cp s3://publicrandomstuff/wordpress.sql.zip /tmp
 unzip /tmp/wordpress.sql.zip
-cat /tmp/wordpress.sql | mysql -u root -ppassword -hrdshostname 
+#cat /tmp/wordpress.sql | mysql -u root -ppassword -hrdshostname 
