00a3ccf0e512a91ded94fa7ffc323b45e564cea0
Hans <hans@cloudlord.com>
2019-06-20 15:21:48 -0700

    Jenkins requires Java8

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 wget -q -O /tmp/jenkins.rpm https://s3-us-west-2.amazonaws.com/cloudlord.com/devops/jenkins-2.181-1.1.noarch.rpm
+yum -y remove java
+yum update
+yum -y install java-1.8.0-openjdk
 rpm -i /tmp/jenkins.rpm 
 service jenkins start
 chkconfig jenkins on
