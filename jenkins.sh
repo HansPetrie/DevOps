9bed278881d518c106f12dfe30bf8d99f0b5885b
Ubuntu <ubuntu@ip-10-21-13-64.us-west-2.compute.internal>
2019-06-20 21:46:02 +0000

    Updated Jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -q -O /tmp/jenkins.rpm https://s3-us-west-2.amazonaws.com/publicrandomstuff/jenkins-1.609.3-1.1.noarch.rpm
+wget -q -O /tmp/jenkins.rpm https://s3-us-west-2.amazonaws.com/cloudlord.com/devops/jenkins-2.181-1.1.noarch.rpm
 rpm -i /tmp/jenkins.rpm 
 service jenkins start
 chkconfig jenkins on
